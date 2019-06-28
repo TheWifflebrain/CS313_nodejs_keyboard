e998c89d4d4b268d9ab2f044f6989f477fd4a84c
TheWifflebrain <blackice150@gmail.com>
2019-06-27 23:28:08 -0600

    test12

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { Pool } = require("pg")
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 const app = express();
+require('util');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
