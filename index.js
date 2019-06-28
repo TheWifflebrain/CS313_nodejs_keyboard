f0be5a3b34e68854605f6d2b3de2085a3f2948d4
TheWifflebrain <blackice150@gmail.com>
2019-06-27 23:07:03 -0600

    test4

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require("pg")
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: connectionString});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/getBoard', getBoard)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+function getBoard(req, res){
+    var id = req.query.id
+
+    getBoardFromDB(id, function(error, result){
+      console.log("Back from db with results", result);
+    });
+
+    res.json(result)
+}
+
+function getBoardFromDB(id, callback){
+  var sql = 'SELECT * FROM keyboard WHERE id = $1::init'
+  var params = [id];
+  pool.query(sql, params, function(err, result){
+    console.log("foun DB result: "+ JSON.stringify(result.rows))
+    callback(null, result.rows);
+  })
+}
