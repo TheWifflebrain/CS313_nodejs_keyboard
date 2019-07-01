f0ece1e7c352b3cfb8d9cb0f27f833096d57fae3
TheWifflebrain <blackice150@gmail.com>
2019-06-30 19:49:31 -0600

    Got everything that i wanted for milestone1

--- public/clientSideHomepage.js
+++ public/clientSideHomepage.js
@@ -6,12 +6,12 @@ function displayKeyboard() {
 		console.log(data);
 
 		for (var i = 0; i < data.list.length; i++) {
-			var keyboard = data.list[i];
-
-			$("#displayKeyboards_id").append("<li>" + keyboard.keyboardname + " " + keyboard.switch + ":" + keyboard.size + "</li>");
+             var keyboard = data.list[i];
+             
+            $("#displayKeyboards_id").empty().append("<div class='jumbotron text-center'><h1>" + keyboard.keyboardname + "</a></h1></div><div class='border keyList'><div><img class='img-thumbnail mx-auto d-block img-fluid' src='" + keyboard.photo + "'></div><div><h4>" + keyboard.descriptionk + "</h4></div><div class='container'><table class='table table-striped'><thead><tr><th>Switch</th><th>Type</th><th>Size</th></tr></thead><tbody><tr><td>" + keyboard.switch + "</td><td>" + keyboard.typek + "</td><td>" + keyboard.size + "</td></tr></tbody></table></div></div>" );
 		}
-
-	})
+    })
+    $("#dKeyboard").toggle();
 }
 
 function displaySwitches() {
@@ -22,12 +22,11 @@ function displaySwitches() {
 		console.log(data);
 
 		for (var i = 0; i < data.list.length; i++) {
-			var switches = data.list[i];
-
-			$("#displaySwitches_id").append("<li>" + switches.switchname + " " + switches.types + ":" + switches.travel + "</li>");
+			 var switches = data.list[i];
+			$("#displaySwitches_id").empty().append("<div class='jumbotron text-center'><h1>" + switches.switchname + "</a></h1></div><div class='border keyList'><div><img class='img-thumbnail mx-auto d-block img-fluid' src='" + switches.photo + "'></div><div></div><div class='container'><table class='table table-striped'><thead><tr><th>Type</th><th>Travel</th><th>Actuation</th><th>Bottom Out</th></tr></thead><tbody><tr><td>" + switches.types + "</td><td>" + switches.travel + "</td><td>" + switches.actuation + "</td><td>" + switches.bottomout + "</td></tr></tbody></table></div></div>" );
 		}
-
-	})
+    })
+    $("#dSwitches").toggle();
 }
 
 function displayCaps() {
@@ -38,10 +37,9 @@ function displayCaps() {
 		console.log(data);
 
 		for (var i = 0; i < data.list.length; i++) {
-			var caps = data.list[i];
-
-			$("#displayCaps_id").append("<li>" + caps.capname + " " + caps.material + ":" + caps.photo + "</li>");
+			 var caps = data.list[i];
+			$("#displayCaps_id").empty().append("<div class='jumbotron text-center'><h1>" + caps.capname + "</a></h1></div><div class='border keyList'><div><img class='img-thumbnail mx-auto d-block img-fluid' src='" + caps.photo + "'></div><div><h4>" + caps.descriptionc + "</h4></div><div class='container'><table class='table table-striped'><thead><tr><th>Material</th></tr></thead><tbody><tr><td>" + caps.material + "</td></tr></tbody></table></div></div>" );
 		}
-
-	})
+    })
+    $("#dCaps").toggle();
 }
