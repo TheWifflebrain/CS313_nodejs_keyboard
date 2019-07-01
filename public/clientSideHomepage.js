49f4817da813a7a5f93e6bf848db7c69554b5308
TheWifflebrain <blackice150@gmail.com>
2019-06-30 18:24:12 -0600

    displaying the switches, cpas, and boards work

--- public/clientSideHomepage.js
+++ public/clientSideHomepage.js
@@ -8,7 +8,39 @@ function displayKeyboard() {
 		for (var i = 0; i < data.list.length; i++) {
 			var keyboard = data.list[i];
 
-			$("#displayKeyboard_id").append("<li>" + keyboard.keyboardName + " " + keyboard.switch + ":" + keyboard.size + "</li>");
+			$("#displayKeyboards_id").append("<li>" + keyboard.keyboardname + " " + keyboard.switch + ":" + keyboard.size + "</li>");
+		}
+
+	})
+}
+
+function displaySwitches() {
+    console.log("In displaySwitches client js");
+    
+	$.get("/getSwitches", function(data) {
+		console.log("Back from the server with:");
+		console.log(data);
+
+		for (var i = 0; i < data.list.length; i++) {
+			var switches = data.list[i];
+
+			$("#displaySwitches_id").append("<li>" + switches.switchname + " " + switches.types + ":" + switches.travel + "</li>");
+		}
+
+	})
+}
+
+function displayCaps() {
+    console.log("In displayCaps client js");
+    
+	$.get("/getCaps", function(data) {
+		console.log("Back from the server with:");
+		console.log(data);
+
+		for (var i = 0; i < data.list.length; i++) {
+			var caps = data.list[i];
+
+			$("#displayCaps_id").append("<li>" + caps.capname + " " + caps.material + ":" + caps.photo + "</li>");
 		}
 
 	})
